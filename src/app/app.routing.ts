00af52b37641c68385b4bbd38d625f6c45d0742f
Md Saney Alam <mdsaney.alam@viacommix.com>
2017-10-03 12:22:17 -0400

    Sidebar Fixed..

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,8 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
-import { TablesComponent } from './tables/tables.component';
-import { TypographyComponent } from './typography/typography.component';
+import { StudentListComponent } from './tables/tables.component';
+import { AddNewStudentComponent } from './typography/typography.component';
 import { IconsComponent } from './icons/icons.component';
 import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
@@ -15,12 +15,11 @@ import { UpgradeComponent } from './upgrade/upgrade.component';
 const routes: Routes =[
     { path: 'dashboard',      component: HomeComponent },
     { path: 'user',           component: UserComponent },
-    { path: 'table',          component: TablesComponent },
-    { path: 'typography',     component: TypographyComponent },
+    { path: 'student-list',   component: StudentListComponent },
+    { path: 'new-student',    component: AddNewStudentComponent },
     { path: 'icons',          component: IconsComponent },
     { path: 'maps',           component: MapsComponent },
     { path: 'notifications',  component: NotificationsComponent },
-    { path: 'upgrade',        component: UpgradeComponent },
       { path: '',          redirectTo: 'dashboard', pathMatch: 'full' }
 ];
 
